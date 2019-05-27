7db7a3c4216e273b2bed3352cb19e378510e8239
Jefferson brito de oliveira <jeffbrito776@gmail.com>
2019-05-27 04:38:58 +0000

    Enviar Novo Arquivo

---
+++ AE-simplify_python/Modelo/Agenda.py
@@ -0,0 +1,27 @@
+class Agenda:
+    __dia=0
+    __semana=0
+    __mes=""
+    __ano=0
+
+#getters/acesso
+    def getDia(self):
+        return self.__dia
+    def getSemana(self):
+        return self.__semana
+    def getMes(self):
+        return self.__mes
+    def getAno(self):
+        return self.__ano
+
+#setters/modificacao
+
+    def setDia(self,dia):
+        self.__dia=dia
+    def setSemana(self,semana):
+        self.__semana=semana
+    def setMes(self,mes):
+        self.__mes=mes
+    def setAno(self,ano):
+        self.__ano=ano
+
