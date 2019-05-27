8bf835e47cf411ca18e59642ab945ed1ffe97e82
Jefferson brito de oliveira <jeffbrito776@gmail.com>
2019-05-27 04:43:04 +0000

    Enviar Novo Arquivo

---
+++ AE-simplify_python/visual2/EntAdm.py
@@ -0,0 +1,13 @@
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+class EntAdm:
+    
+   
+    def __init__(self):
+        builder = Gtk.Builder()
+        builder.add_from_file("../visual2/diaAdm.glade")
+        self.diaAlu = builder.get_object("DiaAdm")
+        builder.connect_signals(self)
+        self.diaAlu.show_all()
+        Gtk.main()
