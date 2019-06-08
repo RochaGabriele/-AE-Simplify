bb11ddb3aa54b81daeca99bebb1760e3122d465a
Jefferson brito de oliveira <jeffbrito776@gmail.com>
2019-06-08 01:18:26 +0000

    Enviar Novo Arquivo

---
+++ AE-simplify_python/modelos/Agenda.py
@@ -0,0 +1,58 @@
+"""
+
+        classe usada para receber os valores de cada sala
+"""
+__author__="José Maik Freitas dos Santos"
+__version__="1.0.0"
+
+
+
+class Agenda:
+    __sala1 = 0
+    __sala2 = 0
+    __sala3 = 0
+    __sala4 = 0
+    __sala5 = 0
+    __sala6 = 0
+    __sala7 = 0
+    __sala8 = 0
+    __sala9 = 0
+#getters/acesso
+    def getSala1(self):
+        return self.__sala1
+    def getSala2(self):
+        return self.__sala2
+    def getSala3(self):
+        return self.__sala3
+    def getSala4(self):
+        return self.__sala4
+    def getSala5(self):
+        return self.__sala5
+    def getSala6(self):
+        return self.__sala6
+    def getSala7(self):
+        return self.__sala7
+    def getSala8(self):
+        return self.__sala8
+    def getSala9(self):
+        return self.__sala9
+#setters/modificacao
+    def setSala1(self,sala1):
+        self.__sala1 = sala1
+    def setSala2(self,sala2):
+        self.__sala2 = sala2
+    def setSala3(self,sala3):
+        self.__sala3 = sala3
+    def setSala4(self,sala4):
+        self.__sala4 = sala4
+    def setSala5(self,sala5):
+        self.__sala5 = sala5
+    def setSala6(self,sala6):
+        self.__sala6 = sala6
+    def setSala7(self,sala7):
+        self.__sala7 = sala7
+    def setSala8(self,sala8):
+        self.__sala8 = sala8
+    def setSala9(self,sala9):
+        self.__sala9 = sala9
+    
