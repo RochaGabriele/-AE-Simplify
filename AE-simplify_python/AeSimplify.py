c032fda232b946216dffaafe13ef7418d47ebbe5
Jefferson brito de oliveira <jeffbrito776@gmail.com>
2019-06-08 01:29:17 +0000

    Enviar Novo Arquivo

---
+++ AE-simplify_python/AeSimplify.py
@@ -0,0 +1,8 @@
+"""
+    modulo utilizada para executar o aplicativo como um todo
+"""
+__author__="Jefferson Brito de Oliveira"
+__version__="1.0.0"
+
+from visual.Entrada import Entrada
+ent = Entrada()
