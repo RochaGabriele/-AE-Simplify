b78e138b2aada9891deda698eb89580641eeb9ee
Jefferson brito de oliveira <jeffbrito776@gmail.com>
2019-06-08 01:45:02 +0000

    Enviar Novo Arquivo

---
+++ Agenda/headers/ui_diaalu.h
@@ -0,0 +1,311 @@
+/********************************************************************************
+** Form generated from reading UI file 'diaalu.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIAALU_H
+#define UI_DIAALU_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_diaalu
+{
+public:
+    QPushButton *butOk;
+    QPushButton *butCan;
+    QLabel *labP;
+    QComboBox *comboBox;
+    QLabel *labS;
+    QComboBox *comboBox_2;
+    QLabel *labTer;
+    QComboBox *comboBox_3;
+    QLabel *labQuar;
+    QComboBox *comboBox_4;
+    QLabel *labelQui;
+    QComboBox *comboBox_5;
+    QLabel *labSex;
+    QComboBox *comboBox_6;
+    QLabel *labSet;
+    QComboBox *comboBox_7;
+    QLabel *labOi;
+    QComboBox *comboBox_8;
+    QLabel *labNo;
+    QComboBox *comboBox_9;
+
+    void setupUi(QDialog *diaalu)
+    {
+        if (diaalu->objectName().isEmpty())
+            diaalu->setObjectName(QString::fromUtf8("diaalu"));
+        diaalu->resize(241, 340);
+        butOk = new QPushButton(diaalu);
+        butOk->setObjectName(QString::fromUtf8("butOk"));
+        butOk->setGeometry(QRect(150, 300, 71, 32));
+        butCan = new QPushButton(diaalu);
+        butCan->setObjectName(QString::fromUtf8("butCan"));
+        butCan->setGeometry(QRect(90, 300, 61, 32));
+        labP = new QLabel(diaalu);
+        labP->setObjectName(QString::fromUtf8("labP"));
+        labP->setGeometry(QRect(10, 20, 131, 16));
+        comboBox = new QComboBox(diaalu);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(140, 10, 76, 26));
+        labS = new QLabel(diaalu);
+        labS->setObjectName(QString::fromUtf8("labS"));
+        labS->setGeometry(QRect(10, 50, 131, 16));
+        comboBox_2 = new QComboBox(diaalu);
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+        comboBox_2->setGeometry(QRect(140, 40, 76, 26));
+        labTer = new QLabel(diaalu);
+        labTer->setObjectName(QString::fromUtf8("labTer"));
+        labTer->setGeometry(QRect(10, 80, 131, 16));
+        comboBox_3 = new QComboBox(diaalu);
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->setObjectName(QString::fromUtf8("comboBox_3"));
+        comboBox_3->setGeometry(QRect(140, 70, 76, 26));
+        labQuar = new QLabel(diaalu);
+        labQuar->setObjectName(QString::fromUtf8("labQuar"));
+        labQuar->setGeometry(QRect(10, 110, 121, 16));
+        comboBox_4 = new QComboBox(diaalu);
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->setObjectName(QString::fromUtf8("comboBox_4"));
+        comboBox_4->setGeometry(QRect(140, 100, 76, 26));
+        labelQui = new QLabel(diaalu);
+        labelQui->setObjectName(QString::fromUtf8("labelQui"));
+        labelQui->setGeometry(QRect(10, 140, 121, 16));
+        comboBox_5 = new QComboBox(diaalu);
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->setObjectName(QString::fromUtf8("comboBox_5"));
+        comboBox_5->setGeometry(QRect(140, 130, 76, 26));
+        labSex = new QLabel(diaalu);
+        labSex->setObjectName(QString::fromUtf8("labSex"));
+        labSex->setGeometry(QRect(10, 170, 121, 16));
+        comboBox_6 = new QComboBox(diaalu);
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->addItem(QString());
+        comboBox_6->setObjectName(QString::fromUtf8("comboBox_6"));
+        comboBox_6->setGeometry(QRect(140, 160, 76, 26));
+        labSet = new QLabel(diaalu);
+        labSet->setObjectName(QString::fromUtf8("labSet"));
+        labSet->setGeometry(QRect(10, 200, 121, 16));
+        comboBox_7 = new QComboBox(diaalu);
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->addItem(QString());
+        comboBox_7->setObjectName(QString::fromUtf8("comboBox_7"));
+        comboBox_7->setGeometry(QRect(140, 190, 76, 26));
+        labOi = new QLabel(diaalu);
+        labOi->setObjectName(QString::fromUtf8("labOi"));
+        labOi->setGeometry(QRect(10, 230, 121, 16));
+        comboBox_8 = new QComboBox(diaalu);
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->addItem(QString());
+        comboBox_8->setObjectName(QString::fromUtf8("comboBox_8"));
+        comboBox_8->setGeometry(QRect(140, 220, 76, 26));
+        labNo = new QLabel(diaalu);
+        labNo->setObjectName(QString::fromUtf8("labNo"));
+        labNo->setGeometry(QRect(10, 260, 111, 16));
+        comboBox_9 = new QComboBox(diaalu);
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->addItem(QString());
+        comboBox_9->setObjectName(QString::fromUtf8("comboBox_9"));
+        comboBox_9->setGeometry(QRect(140, 250, 76, 26));
+
+        retranslateUi(diaalu);
+
+        QMetaObject::connectSlotsByName(diaalu);
+    } // setupUi
+
+    void retranslateUi(QDialog *diaalu)
+    {
+        diaalu->setWindowTitle(QApplication::translate("diaalu", "Dialog", nullptr));
+        butOk->setText(QApplication::translate("diaalu", "OK", nullptr));
+        butCan->setText(QApplication::translate("diaalu", "Cancelar", nullptr));
+        labP->setText(QApplication::translate("diaalu", "Digite a primeira Sala:", nullptr));
+        comboBox->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labS->setText(QApplication::translate("diaalu", "Digite a segunda Sala:", nullptr));
+        comboBox_2->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_2->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_2->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_2->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_2->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_2->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_2->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_2->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_2->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labTer->setText(QApplication::translate("diaalu", "Digite a terceira Sala:", nullptr));
+        comboBox_3->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_3->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_3->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_3->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_3->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_3->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_3->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_3->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_3->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labQuar->setText(QApplication::translate("diaalu", "Digite a quarta Sala:", nullptr));
+        comboBox_4->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_4->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_4->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_4->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_4->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_4->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_4->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_4->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_4->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labelQui->setText(QApplication::translate("diaalu", "Digite a quinta Sala:", nullptr));
+        comboBox_5->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_5->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_5->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_5->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_5->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_5->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_5->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_5->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_5->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labSex->setText(QApplication::translate("diaalu", "Digite a sexta sala:", nullptr));
+        comboBox_6->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_6->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_6->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_6->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_6->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_6->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_6->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_6->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_6->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labSet->setText(QApplication::translate("diaalu", "Digite a s\303\251tima sala:", nullptr));
+        comboBox_7->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_7->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_7->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_7->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_7->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_7->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_7->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_7->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_7->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labOi->setText(QApplication::translate("diaalu", "Digite a oitava sala:", nullptr));
+        comboBox_8->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_8->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_8->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_8->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_8->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_8->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_8->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_8->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_8->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+        labNo->setText(QApplication::translate("diaalu", "Digite a nona sala:", nullptr));
+        comboBox_9->setItemText(0, QApplication::translate("diaalu", "1", nullptr));
+        comboBox_9->setItemText(1, QApplication::translate("diaalu", "2", nullptr));
+        comboBox_9->setItemText(2, QApplication::translate("diaalu", "3", nullptr));
+        comboBox_9->setItemText(3, QApplication::translate("diaalu", "4", nullptr));
+        comboBox_9->setItemText(4, QApplication::translate("diaalu", "5", nullptr));
+        comboBox_9->setItemText(5, QApplication::translate("diaalu", "6", nullptr));
+        comboBox_9->setItemText(6, QApplication::translate("diaalu", "7", nullptr));
+        comboBox_9->setItemText(7, QApplication::translate("diaalu", "8", nullptr));
+        comboBox_9->setItemText(8, QApplication::translate("diaalu", "9", nullptr));
+
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class diaalu: public Ui_diaalu {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIAALU_H
